837404158b005078e1f82c0576fcb29ebe654fb3
Agus Bira <agusbira398@gmail.com>
2025-05-12 23:40:52 -0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = {
-  images: {
-    domains: ['tudominio.com', 'images.unsplash.com'], // Dominios permitidos
-  },
+const nextConfig = {
+    experimental: {
+        appDir: true,
+        serverActions: true,
+    },
+    images: {
+        domains: ['res.cloudinary.com', 'cdn.discordapp.com', 'i.imgur.com'],
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'standalone',
+    compiler: {
+        styledComponents: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
+
 export default nextConfig;
