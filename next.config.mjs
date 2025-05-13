b540e4ffb2e2c6966a2f62d89e5c8c3ee89a5faf
Agus Bira <agusbira398@gmail.com>
2025-05-12 23:19:33 -0300

    agrego tecnologias

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+module.exports = {
+  images: {
+    domains: ['tudominio.com', 'images.unsplash.com'], // Dominios permitidos
+  },
+};
 export default nextConfig;
